086cb4d1460b3d3b8151dd09aa9159bccc5ffabb
ayushyogi0 <157874851+ayushyogi0@users.noreply.github.com>
2025-05-01 22:35:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let seconds = 0o0;
+let tens = 0o0;
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+let interval;
+
+btnStart.addEventListener('click', () => {
+    interval = setInterval(startTimer,10)
+    
+
+})
+btnStop.addEventListener('click',() => {
+    clearInterval(interval);
+
+})
+btnReset.addEventListener('click',()=>{
+    clearInterval(interval);
+    tens = '00';
+    seconds = '00';
+    getTens.innerHTML=tens;
+    getSeconds.innerHTML=seconds;
+})
+
+
+function startTimer(){
+    tens++;
+    if(tens <=9){
+        getTens.innerHTML ='0' + tens;
+    }
+    if(tens >9){
+        getTens.innerHTML = tens;
+    }
+    if(tens >99){
+        seconds++;
+        getSeconds.innerHTML= '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if(seconds > 9){
+        getSeconds.innerHTML =seconds;
+        
+    }
+}
+
+
+
